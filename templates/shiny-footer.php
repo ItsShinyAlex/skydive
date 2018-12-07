bf3f81ef156b3d34797d1bd74b40ef135dad810d
Lyngall <soporte@esbrillante.com>
2018-12-06 21:20:38 -0600

    transpoorte desktop finished

---
+++ templates/shiny-footer.php
@@ -0,0 +1,137 @@
+
+
+   <section id="contacto">
+    <div class="container">
+      <div class="flex">
+          <div class="col">
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      Dirección aeródromo de Tequesquitengo
+                        Emiliano zapata #19 San José Vista Hermosa, Puente de Ixtla, Morelos
+                        Cp. 62680
+                  </p>
+              </div>
+                 
+             </div>
+             
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      Oficina en Cuernavaca
+                        Paracaidismo México AC
+                        Av. De los 50 metros #100
+                        Col villas deportivas
+                        Torre III oficina 406
+                        Cp. 62389
+                        Cuernavaca, Morelos
+
+                  </p>
+              </div>
+                 
+             </div>
+             
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      
+                        Horarios de atención
+                        Lunes a Domingo: 8:00 a 19:00
+                  </p>
+              </div>
+                 
+             </div>
+             
+             
+              
+          </div>
+          <div class="col">
+              
+              <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      
+                        Correo electrónico
+                        contacto@paracaidismo.mx
+                  </p>
+              </div>
+                 
+             </div>
+             
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      
+                        Télefono y Whatsapp México
+                        55 4044 0422 y 55 7397 0829
+                  </p>
+              </div>
+                 
+             </div>
+             
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      
+                        Oficina en Cuernavaca
+                        01 777 171 5650
+                  </p>
+              </div>
+                 
+             </div>
+             
+             <div class="flex">
+                 
+                 <div class="smallc col">
+                  <img src="../img/footer/" alt="">
+               </div>
+              <div class="bigc col">
+                  <p>
+                      
+                        /skydivemexico
+                  </p>
+              </div>
+                 
+             </div>
+              
+          </div>
+      </div>
+            
+         
+        
+                       
+    </div>
+    
+    <!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+
+</script>
+<!--End of Zendesk Chat Script-->
+
+</section>
+
+</section>
---
+++ templates/transporte.php
@@ -0,0 +1,56 @@
+<?php
+    /* Template Name:  transporte*/ 
+    get_header();
+?>
+<div id="tornado-landing">
+
+<section class="template-section" id="atusalto">
+    <div class="container">
+        <div class="flex fcentered">
+        
+            <div class="col">
+                
+            </div>
+            <div class="col left-allign">
+                <h1 class="white heavy" >
+                    <span class="normal">Te llevamos directo</span>  <br>
+                    <span class="orange loved">
+                         A TU SALTO <br>
+                    </span>
+                    <span class="big noodle">EN TEQUESQUITENGO</span> 
+                </h1>
+             
+                    
+                <p class="">Disfruta de nuestras salidas en Perisur los Sábados y Domingos a las 8 AM.</p>
+                <p class="mar-bot">Viaje redondo: $420</p>
+                <a href="" class="orangeb btn-new">RESERVAR</a>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="template-section" id="contacto-parachutes">
+    <div class="container fcentered">
+        <div class="w90">
+           
+           <div class="">
+               <h2 class="navy loved left">CONTACTO</h2>
+               
+               <div id="cform">
+                     <?php echo do_shortcode("[caldera_form id='CF58ec0a56e1dab']"); ?>
+                </div>
+               
+           </div>
+            
+        </div>
+    </div>
+</section>
+
+
+</div>
+
+<?php 
+    get_footer();
+?>
+
