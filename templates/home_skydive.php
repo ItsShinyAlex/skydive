4098d42f8278ff1d1a8210a2aa075e8f08b1f98c
Lyngall <soporte@esbrillante.com>
2018-12-18 18:05:18 -0600

    not important

---
+++ templates/home_skydive.php
@@ -0,0 +1,104 @@
+<?php
+    /* Template Name:  home*/ 
+    get_header();
+?>
+<div id="tornado-landing">
+
+<section class="template-section top2col" id="home-banner">
+    <div class="container">
+        <div class="flex">
+        
+           
+           
+            <div class="col left-allign ">
+            </div>
+            
+            <div class="col left-allign">
+                <h1 class="white heavy" >
+                    <span class="normal">¿Cómo llegar a </span>  <br>
+                    <span class="orange loved">
+                            SKYDIVE MÉXICO <br>
+                    </span>
+                    <span class="big noodle">TEQUESQUITENGO?</span> 
+                </h1>
+                       
+                <a href="https://www.google.com/maps/place/Skydive+M%C3%A9xico+-+Paracaidismo+Tequesquitengo/@18.647693,-99.264669,15z/data=!4m2!3m1!1s0x0:0x2be027abb591c6c7?ved=2ahUKEwiX6cfK7p3fAhVLs6wKHfGzDqEQ_BIwFXoECAYQCA" class="social-container twitter" target="_blank">
+                    <div class="social-cube">
+                      <div class="front center">
+                        VER UBICACIÓN EN <br>
+                        GOOGLE MAPS
+                      </div>
+                      <div class="bottom center">
+                         <i class="fas fa-map-marker-alt "></i>
+                      </div>
+                    </div>
+                  </a>
+                
+                
+            </div>
+     
+        </div>
+    </div>
+    
+    <div class="tobottom">
+        <div class="skew-cc">
+
+        
+        </div>
+        
+    </div>
+    
+    
+</section>
+
+<section class="template-section top2col blue-skew" id="tandem">
+    <div class="container">
+        <div class="flex">
+        
+           
+           
+            
+            
+            <div class="col left-allign">
+                <h1 class="white heavy" >
+                    <span class="normal">¿Cómo llegar a </span>  <br>
+                    <span class="orange loved">
+                            SKYDIVE MÉXICO <br>
+                    </span>
+                    <span class="big noodle">TEQUESQUITENGO?</span> 
+                </h1>
+                       
+                <a href="https://www.google.com/maps/place/Skydive+M%C3%A9xico+-+Paracaidismo+Tequesquitengo/@18.647693,-99.264669,15z/data=!4m2!3m1!1s0x0:0x2be027abb591c6c7?ved=2ahUKEwiX6cfK7p3fAhVLs6wKHfGzDqEQ_BIwFXoECAYQCA" class="social-container twitter" target="_blank">
+                    <div class="social-cube">
+                      <div class="front center">
+                        VER UBICACIÓN EN <br>
+                        GOOGLE MAPS
+                      </div>
+                      <div class="bottom center">
+                         <i class="fas fa-map-marker-alt "></i>
+                      </div>
+                    </div>
+                  </a>
+                
+                
+            </div>
+            
+            
+            <div class="col left-allign ">
+            </div>
+            
+     
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<?php 
+	include 'shiny-footer.php';
+    get_footer();
+?>
+
