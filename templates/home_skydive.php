3f51111d9cfa148fc50b3d70da9b38e2a44c83f3
Lyngall <soporte@esbrillante.com>
2018-12-19 19:28:16 -0600

    Advance

--- templates/home_skydive.php
+++ templates/home_skydive.php
@@ -61,7 +61,7 @@
            
             
             
-            <div class="col left-allign">
+            <div class="col center">
                 <h1 class="white heavy center" >
                     
                      <span class="big noodle">SALTA EN CAÍDA LIBRE</span> <br>
@@ -77,7 +77,7 @@
                    
                 </h1>
                        
-                <img src="../img/home/cloudsSatlo.png" alt="Salto desde las nubes">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/cloudsSatlo.png" alt="Salto desde las nubes">
                 
                 
             </div>
@@ -101,6 +101,200 @@
     </div>
 </section>
 
+
+
+<section class="template-section" id="avientate">
+    <div class="container">
+       
+       <div class="fcentered">
+           
+                  <h2 class="center">
+                      <span class="big noodle">¡Aviéntate tu también! </span> <br>
+                  </h2>
+                  
+                  <p>Estas son las pruebas de quienes ya lo hicieron</p>
+                  
+
+                  <div class="flex">
+
+                <div class="hover06 col">
+
+
+                      <div class="">
+
+
+                          <figure>
+                           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/avienta1.jpg" alt="">
+                       </figure>
+
+                      </div>
+
+                </div>
+
+               <div class="hover06 col">
+
+
+                      <div class="">
+
+
+                          <figure>
+                           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/avienta2.jpg" alt="">
+                       </figure>
+
+                      </div>
+
+                </div>
+
+                <div class="hover06 col">
+
+
+                      <div class="">
+
+
+                          <figure>
+                           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/avienta3.jpg" alt="">
+                       </figure>
+
+                      </div>
+
+                </div>
+
+                <div class="hover06 col">
+
+
+                      <div class="">
+
+
+                          <figure>
+                           <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/avienta4.jpg" alt="">
+                       </figure>
+
+                      </div>
+
+                </div>
+
+            </div>
+           
+           
+           <a href="https://www.google.com/maps/place/Skydive+M%C3%A9xico+-+Paracaidismo+Tequesquitengo/@18.647693,-99.264669,15z/data=!4m2!3m1!1s0x0:0x2be027abb591c6c7?ved=2ahUKEwiX6cfK7p3fAhVLs6wKHfGzDqEQ_BIwFXoECAYQCA" class="social-container twitter" target="_blank">
+                    <div class="social-cube">
+                      <div class="front center navy-back">
+                        QUIERO SALTAR
+                      </div>
+                      <div class="bottom center">
+                         <i class="fas fa-plane"></i>
+                      </div>
+                    </div>
+                  </a>
+           
+           
+       </div>
+       
+     
+    </div>
+</section>
+
+<section class="template-section top2col" id="why1">
+    <div class="container">
+        <div class="flex">
+        
+            <div class="col center">
+                <h2 class="black heavy" >
+                   <span class="normal orange">¿Por qué</span><br>
+                    <span class="white big noodle">Skydive México<br> Tequesquitengo </span>  <br>
+                    <span class="normal orange"> es el centro de paracaidismo número uno del mundo en </span><br>
+                    <span class="gold loved">
+                            SALTOS TANDEM? <br>
+                    </span>
+                    
+                </h2>         
+       
+                
+            </div>
+           
+           
+            <div class="col left-allign ">
+                
+                <p>
+                    Skydive México cuenta con un <span class="orange">récord perfecto de seguridad</span> , esto gracias al puntual mantenimiento de nuestras aeronaves, invertimos en los mejores equipos TANDEM a nivel mundial.
+                </p>
+                
+                <p>Tu seguridad es nuestra principal prioridad
+                Para lograr altos estándares de seguridad no escatimamos gastos en mantenimientos de aviones, equipos, capacitación a instructores y personal de apoyo.</p>
+                
+            </div>
+            
+            
+     
+        </div>
+        
+        <div class="flex flex3">
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/1why1.png" alt="">
+                <h3>Saltos <br>
+                seguros</h3>
+                <p>Nuestros equipos tienen un dispositivo de seguridad llamado VIGIL, que abre el paracaídas de emergencia automáticamente si es necesario.</p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/2why1.png" alt="">
+                <h3>Instructores <br>
+                Certificados</h3>
+                <p>
+                    
+                    Instructores certificados y súper experimentados. Para garantizar tu seguridad todos los instructores de Skydive México pasan por una serie de rigurosas capacitaciones.
+                    
+                </p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/3why1.png" alt="">
+                <h3>Aviones <br>
+                Dedicados</h3>
+                <p>Nuestros aviones están especialmente modificados con base en las regulaciones para la práctica del paracaidismo.</p>
+                
+                <p>Son los más seguros por sus modernos motores nuevos tipo Turbo-Hélice, los más rápidos y los más económicos.</p>
+            </div>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        <div class="flex flex3">
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/4why1.png" alt="">
+                <h3>Preciosa vista del lago <br>
+                Tequesquitengo</h3>
+                <p>Disfrutaras de una hermosa vista del lago de Tequesquitengo, que te hará vivir una experiencia única.</p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/5why1.png" alt="">
+                <h3>Vídeo Digital <br>
+                en Caída Libre</h3>
+                <p>
+                    
+                    Revive esta experiencia, contratando el servicio de Vídeo Digital en donde se capta la secuencia de todo tu salto: ¡Desde la preparación hasta tu aterrizaje! 
+                    
+                </p>
+            </div>
+            <div class="col">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/6why1.png" alt="">
+                <h3>Precios <br>
+                accesibles</h3>
+                <p>Contamos con aeronoves e instalaciones propias, lo que nos permite ofrecer un servicio de calidad internacional al precio más económico del mundo.
+                </p>
+
+            </div>
+        </div>
+        
+        
+    </div>
+    
+
+    
+</section>
 
 
 
