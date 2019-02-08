e24ef7adf755d9eb23de99c1a175f8c76475d913
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-02-08 00:03:56 -0600

    academia_primeraparte

---
+++ templates/academia.php
@@ -0,0 +1,136 @@
+<?php
+    /* Template Name:  academia*/ 
+    get_header();
+?>
+<div class="curso">
+<section class="template-section" id="slider_academia">
+    <div class="container">
+    <h2>Curso de paracaidismo </h2>
+    <h3>¡SALTA sin instructor!</h3>
+    <p>En el centro de Paracaidismo de Tequesquitengo, Skydive México y Academia Paracaidismo ofrecen cursos de caída libre acelerada (AFF). Tu seguridad y aprendizaje son nuestros objetivos principales.</p>
+    <a href="#" class="btn-new " target="_blank" >Me quiero registrar</a>
+    </div>
+</section>
+
+<section class="template-section" id="niveles">
+<div class="container">
+
+<h2>7 niveles (salto) de aprendizaje</h2>
+<div class="grid">
+    <div class="col2">
+        <h3>NIVEL 1</h3>
+        <p>Clase teórica Tándem AFF
+Demostrar conciencia de altura, apertura del paracaídas 
+y patrones de aterrizaje.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 2</h3>
+        <p>Relájate, mira al horizonte, observa a tu instructor, checa tu altímetro y abre tu paracaídas.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 3</h3>
+        <p>Relájate, realiza giros y avances, mantén conciencia de altura y abre tu paracaídas.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 4</h3>
+        <p>En este nivel empiezas a volar sólo/a manteniendo control de dirección.</p>
+    </div>
+    
+</div>
+<div class="grid">
+    <div class="col2">
+        <h3>NIVEL 5</h3>
+        <p>Salida sin asistencia de clavado, back loop y prácticas con el paracaídas abierto.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 6</h3>
+        <p>Salida sin asistencia con loop, prácticas de giros y avances.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 7</h3>
+        <p>Salida sin asistencia, prácticas de giros, deltas y niveles de vuelo.</p>
+    </div>
+    <div class="col2">
+        <h3>NIVEL 4</h3>
+        <p>Programa una fecha con solo tu identificación oficial. </p>
+        <p>Solo mayores de 18</p>
+    </div>
+    
+</div>
+<h3>SE INCLUYEN PRÁCTICAS EN EL TUNEL DE VIENTO TORNADO.</h3>
+<p>Estas prácticas, además de aumentar tu seguridad y confianza en caída libre, te ayudan a progresar mucho más rápido en tu curso de paracaidismo. Aparte de ser una actividad divertida y única 
+en México.</p>
+ </div>
+</section>
+
+<section class="template-section" id="precio">
+<div class="container">
+<h2>PRECIO</h2>
+<table id="customers">
+  <tr>
+    <th>Salto / Nivel</th>
+    <th>Cuerpo de Paracaidismo <br> Pagos Parciales</th>
+    <th>Curso Intensivo <br> O pago de contado</th>
+    <th>Cuerpo de Paracaidismo mujer</th>
+  </tr>
+  <tr class="amarillo">
+    <td>Teoría / Nivel 1</td>
+    <td>$3600</td>
+    <td>$3400</td>
+    <td>$2900</td>
+  </tr>
+  <tr class="azul">
+    <td>Nivel 2</td>
+    <td>$3600</td>
+    <td>$3400</td>
+    <td>$2900</td>
+  </tr>
+  <tr class="amarillo">
+    <td>Nivel 3</td>
+    <td>$3600</td>
+    <td>$3400</td>
+    <td>$2900</td>
+  </tr>
+  <tr class="azul">
+    <td>Nivel 4</td>
+    <td>$3500</td>
+    <td>$3400</td>
+    <td>$2800</td>
+  </tr>
+  <tr class="amarillo">
+    <td>Nivel 5</td>
+    <td>$3400</td>
+    <td>$3200</td>
+    <td>$2800</td>
+  </tr>
+  <tr class="azul">
+    <td>Nivel 6</td>
+    <td>$3400</td>
+    <td>$3100</td>
+    <td>$2800</td>
+  </tr>
+  <tr class="amarillo">
+    <td>Nivel 7</td>
+    <td>$3400</td>
+    <td>$3100</td>
+    <td>$2800</td>
+  </tr>
+  <tr class="rojo">
+    <td>Precio Total del Curso</td>
+    <td>$24500</td>
+    <td>$23000</td>
+    <td>$19900</td>
+  </tr>
+  
+</table>
+<p>CURSO PARA MUJERES. Actualmente contamos con un descuento especial para chicas. La Academia de Paracaidismo, 
+en su visión por fomentar el deporte en las mujeres, decidió promover estos precios de promoción. Comunícate al 7771096727 para más información y agendar tu curso (WhatsApp).</p>
+<a href="#" class="btn-new " target="_blank" >¿A que se refiere, el curso intensivo?</a>
+ </div>
+</section>
+
+</div>
+<?php 
+
+    get_footer();
+?>
