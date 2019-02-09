35a2308920a9331f5cf51dffcf73ea365750d6c1
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2019-02-08 19:22:41 -0600

    academia_parte2

--- templates/academia.php
+++ templates/academia.php
@@ -19,20 +19,24 @@
 <div class="grid">
     <div class="col2">
         <h3>NIVEL 1</h3>
+        <hr>
         <p>Clase teórica Tándem AFF
 Demostrar conciencia de altura, apertura del paracaídas 
 y patrones de aterrizaje.</p>
     </div>
     <div class="col2">
         <h3>NIVEL 2</h3>
+        <hr>
         <p>Relájate, mira al horizonte, observa a tu instructor, checa tu altímetro y abre tu paracaídas.</p>
     </div>
     <div class="col2">
         <h3>NIVEL 3</h3>
+        <hr>
         <p>Relájate, realiza giros y avances, mantén conciencia de altura y abre tu paracaídas.</p>
     </div>
     <div class="col2">
         <h3>NIVEL 4</h3>
+        <hr>
         <p>En este nivel empiezas a volar sólo/a manteniendo control de dirección.</p>
     </div>
     
@@ -40,20 +44,23 @@ y patrones de aterrizaje.</p>
 <div class="grid">
     <div class="col2">
         <h3>NIVEL 5</h3>
+        <hr>
         <p>Salida sin asistencia de clavado, back loop y prácticas con el paracaídas abierto.</p>
     </div>
     <div class="col2">
         <h3>NIVEL 6</h3>
+        <hr>
         <p>Salida sin asistencia con loop, prácticas de giros y avances.</p>
     </div>
     <div class="col2">
         <h3>NIVEL 7</h3>
+        <hr>
         <p>Salida sin asistencia, prácticas de giros, deltas y niveles de vuelo.</p>
     </div>
-    <div class="col2">
-        <h3>NIVEL 4</h3>
-        <p>Programa una fecha con solo tu identificación oficial. </p>
-        <p>Solo mayores de 18</p>
+    <div class="col2 programa">
+        
+        <p>Programa una <span>fecha</span> con solo tu identificación <span>oficial</span> </p>
+        <p class="mayores">Solo mayores de 18</p>
     </div>
     
 </div>
@@ -66,7 +73,7 @@ en México.</p>
 <section class="template-section" id="precio">
 <div class="container">
 <h2>PRECIO</h2>
-<table id="customers">
+<table >
   <tr>
     <th>Salto / Nivel</th>
     <th>Cuerpo de Paracaidismo <br> Pagos Parciales</th>
@@ -129,7 +136,402 @@ en su visión por fomentar el deporte en las mujeres, decidió promover estos pr
  </div>
 </section>
 
+<section class="template-section" id="iniciar">
+   <div class="container">
+   <h2>COMO INICIAR EL CURSO</h2>
+   <p>El primer paso es programar una fecha para iniciar el curso teórico. Deberás traer a nuestras instalaciones únicamente una identificación oficial, después de este entrenamiento en tierra, ya podrás comenzar con los niveles del curso. Debemos programar una fecha que te convenga para iniciar el curso, de preferencia por WhatsApp (7771096727). Para los saltos, se recomienda traer ropa cómoda deportiva. La edad mínima para realizar el curso son 18 años y el peso máximo 90 Kg.</p>
+   <p>EL CURSO ES MUY PERSONALIZADO! Podemos empezar cuando gustes, abrimos los 365 días del año.</p>
+    </div>
+    </section>
+
+<section class="template-section" id="horario">
+   <div class="container">
+   <h2>HORARIOS, FECHAS Y DURACIÓN</h2>
+   <p>Toma en cuenta que la duración de la clase teórica es de 3 a 4 horas. Los saltos se podrán efectuar de lunes a viernes, de 11h00 a 14h00, o sábados y domingos de 9h00 a 17h00. ¡Lo ideal es que realices el primer nivel del curso el día de la teoría o inmediatamente el día después! <br>
+Considera que entre semana podrás hacer UN salto por día, o quizá 2 en función de las reservaciones, y en fin de semana se podrán realizar hasta 4 saltos máximo por día.</p>
+   <p>Sobre la duración del curso, realmente depende de cada alumno. Puedes realizar más de un salto diario! O saltar una vez por semana o cada quince días, esto depende de tu disponibilidad. </p>
+   <div class="grid">
+    <div class="col">
+      <p>LA DURACIÓN DEL CURSO 
+depende de cada alumno.</p>
+      <p>Puedes por ejemplo, tomar vacaciones 
+y terminar el curso en una semana. O por ejemplo, se puede tomar el curso en 3 días!. Esto se llama Curso Intensivo y es más accesible, como se muestra en la tabla.</p>
+      <p>Viernes : Teoría y 1er nivel.<br>
+Sábado : Niveles 2, 3 y 4.<br>
+Domingo : Niveles 5, 6 y 7.
+ </p>
+      <p>Tu espacio en nuestra zona de salto es importante. El día que quieras saltar y a la hora que gustes, siempre habrá espacio para tu llegada. </p>
+       </div>
+       <div class="col">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/persona.png" alt="persona">
+       </div>
+       </div>
+       <p>Sobre la certificación, tienes la opción de adquirir una licencia internacional tipo A, la cual está reconocida a nivel mundial. La Bitácora de salto la llevas a partir del primer NIVEL, y la licencia se otorga cuando cumples 25 saltos en total (contando los 7 del curso) demostrando ciertas habilidades. Toma en cuenta que el primer paso para ser paracaidista es terminar satisfactoriamente los 7 niveles del curso. </p>
+    </div>
+    </section>
+<section class="template-section" id="contamos">
+  <div class="container">
+   <h2>ADEMÁS, CONTAMOS CON:</h2>
+   <div class="grid">
+    <div class="col2">
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/hospedaje.png" alt="persona">
+       <p>HOSPEDAJE</p>
+       </div>
+       <div class="col2">
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/alberca.png" alt="alberca">
+       <p>ALBERCA</p>
+       </div>
+       <div class="col2">
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/restauante.png" alt="restauante">
+       <p>RESTAURANTE</p>
+       </div>
+       <div class="col2">
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/campingzone.png" alt="campingzone">
+       <p>CAMPING ZONE</p>
+       </div>
+    </div>
+    </div>
+    </section>
+    
+    <section class="template-section" id="aviones">
+    <div class="container">
+    <h2>¡Los mejores aviones de paracaidismo!</h2>
+       <p>Contamos con tres aviones Cessna Caravan Super Van 900 
+y un Super Twin Otter. Son lo más avanzado para el paracaidismo 
+a nivel mendial ¡Y los tenemos aquí para ti!</p>
+        </div>
+    </section>
+
+
+
+
+<section class="template-section" id="faq-arrive">
+
+    <div class="container">
+         <h2>PREGUNTAS FRECUENTES</h2>
+       <div class="fcentered">  
+                              
+                 <div class="card">
+                      
+                      <article class="bdark">
+                        <h4 class="effect" data-accordion-element-trigger> ¿Qué incluye el curso de paracaidismo?</h4>
+                        <div data-accordion-element-content class="content">
+                           <p class="pad-top">Es curso te incluye todos los accesorios necesarios para tu adiestramiento, equipo, casco, altímetro, goles, overol, avión, instructor, etc … así como las clases teóricas correspondientes. </p>
+                           </div>
+                      </article>
+                      
+                      <article class="bnavy">
+                        <h4 class="effect" data-accordion-element-trigger>¿Cuáles son las Formas de pago? </h4>
+                        <div data-accordion-element-content class="content">
+                         
+                          <p class="pad-top">Se puede pagar con tarjeta de crédito DEBITO o CREDITO (consulta restricciones) excepto AMEX, o de preferencia en efectivo. Se  recomienda que vayas pagando POR NIVEL, el mismo día del salto.</p>
+                         
+                        </div>
+                      </article>
+                      
+                      <article class="bdark">
+                        <h4 class="effect" data-accordion-element-trigger>¿Qué tipo de Certificación?</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">Tienes la opción de tramitar una licencia que está reconocida a nivel mundial (tipo A). Sin embargo una bitácora de salto (que es un documento oficial firmada por tu instructor y el sello de la academia) la llevas a partir del primer NIVEL, y la licencia se tramita cuando cumples 25 saltos en total, demostrando ciertas habilidades. Toma en cuenta que el primer paso para ser paracaidista es terminar satisfactoriamente los 7 niveles del curso.</p>
+                       
+                        </div>
+                      </article>
+                   
+                                   
+                   <article class="bnavy">
+                        <h4 class="effect" data-accordion-element-trigger>¿Cuál es la Duración del curso?</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">Depende de ti. Puedes realizar más de un salto diario! O saltar una vez por semana, esto depende de tu disponibilidad. ¡Abrimos los 365 días del año!</p>
+                       
+                          
+                        </div>
+                      </article>
+                      
+                        <article class="bdark">
+                        <h4 class="effect" data-accordion-element-trigger>¿Es necesario depositar?</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">No es necesario realizar ningún depósito para reservar o programar tu llegada y comenzar el curso. Simplemente acordamos una fecha y hora que te convenga (7771096727) y te esperamos para empezar tu adiestramiento como paracaidista.</p>
+                       
+                        </div>
+                      </article>
+                   
+                                   
+                   <article class="bnavy">
+                        <h4 class="effect" data-accordion-element-trigger>¿Cómo reservar?</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">Solo mándame un WhatsApp al teléfono (7771096727) para confirmar el día que quieres empezar. Se pide que por lo menos nos avises 1 día antes de la fecha de inicio.</p>
+                       
+                          
+                        </div>
+                      </article>
+                        <article class="bdark">
+                        <h4 class="effect" data-accordion-element-trigger>¿Cuándo puedo iniciar el curso?</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">Podemos iniciar cuando gustes. Es un curso muy personalizado y las fechas las pone el alumno. </p>
+                       
+                        </div>
+                      </article>
+                    </div>
+                    
+       </div>
+       
+        
+        
+        
+      
+    </div>
+ 
+</section>
+
+    <section class="template-section" id="experimentados">
+    <div class="container">
+    <h2>EXPERIMENTADOS Y PARACAIDISTAS PROFESIONALES</h2>
+       <p>La Academia de Paracaidismo ofrece los precios más accesibles para paracaidistas profesionales. Sin mencionar el fantástico túnel de viento que está localizado en nuestras instalaciones. Este aparato ayuda a perfeccionar tun vuelo en caída libre además de ser la base para desarrollar habilidades extraordinarias. Algunas ventajas son:</p>
+       
+       <ul>
+           
+           <li>Volamos todos los días del año</li>
+           <li>Contamos con los aviones más modernos para la práctica de este deporte con vanguardia internacional. Nuestros modelos incluye saltos de Cessna Caravan 208, Supervan 900 y Supe Twin Otter DHC-6.</li>
+           <li>Contamos con los equipos de renta propios más modernos.</li>
+           <li>Contamos area de camping, dormitorios, alberca, áreas verdes, estacionamiento, etc …</li>
+           <li>El clima es ideal para la práctica de este deporte.</li>
+       </ul>
+       
+       <table >
+ 
+  <tr class="amarillo">
+    <td>Salto Individual</td>
+    <td>$470</td>
+    
+  </tr>
+  <tr class="azul">
+    <td>Salto en paquete</td>
+    <td>$430</td>
+   
+  </tr>
+  <tr class="amarillo">
+    <td>Hop / Pop</td>
+    <td>$380</td>
+   
+  </tr>
+  <tr class="azul">
+    <td>Práctica en Tunel de Viento <br> (2 minutos) </td>
+    <td>$260</td>
+   
+  </tr>
+  <tr class="amarillo">
+    <td>Renta del equipo por salto</td>
+    <td>$300</td>
+   
+  </tr>
+  <tr class="azul bold">
+    <td>Renta del equipo por día</td>
+    <td>$1000</td>
+
+  </tr>
+ 
+</table>
+       
+        </div>
+    </section>
+ <section class="template-section" id="contacto">
+    <div class="container">
+    <h2>¡Contáctanos hoy mismo para comenzar esta aventura!</h2>
+    <div class="grid">
+        <div class="col con1">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/telefono.png" alt="telefono">
+        <p> Solo curso de paracaidismo <br>
+(+52) 777 109 6727<br> (WHATSAPP)</p>
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/academia/E-mail.png" alt="E-mail">
+        <p> skydivemexico@gmail.com</p>
+        </div>
+         <div class="col">
+             <?php echo do_shortcode( '[caldera_form id="CF5c5e20b4623a8"]' ); ?>  
+         </div>
+    </div>
+     </div>
+    </section>
+
+
+
+
 </div>
+
+
+
+<script>
+    var gianniAccordion = (function(){
+      return class {      
+
+        transitionendEventName(){
+          var i,
+              el = document.createElement('div'),
+              transitions = {
+                'transition': 'transitionend',
+                'OTransition': 'otransitionend',
+                'MozTransition': 'transitionend',
+                'WebkitTransition': 'webkitTransitionEnd'
+              };
+
+          for (i in transitions) {
+            if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+              return transitions[i];
+            }
+          }
+        }
+
+        expand(el){
+          function resetHeight(ev){
+            if(ev.target != el.content) return
+            el.content.removeEventListener(this.transitionendevent, bindEvent);
+
+            if(!el.isOpen) return
+
+            requestAnimationFrame(()=>{      
+              el.content.style.transition = '0';
+              el.content.style.height = 'auto';
+
+              requestAnimationFrame(()=>{
+                el.content.style.height = null;
+                el.content.style.transition = null;
+
+                this.fire("elementOpened", el);
+              });
+            });
+          }
+
+          var bindEvent = resetHeight.bind(this);
+          el.content.addEventListener(this.transitionendevent, bindEvent);
+
+          el.isOpen = true;
+          el.content.style.height = el.content.scrollHeight + "px";
+        }
+
+        collapse(el){
+
+          function endTransition(ev){     
+            if(ev.target != el.content) return
+            el.content.removeEventListener(this.transitionendevent, bindEvent);
+
+            if(el.isOpen) return
+
+            this.fire("elementClosed", el);
+          }
+
+          var bindEvent = endTransition.bind(this);
+          el.content.addEventListener(this.transitionendevent, bindEvent);
+
+          el.isOpen = false;    
+
+          requestAnimationFrame(()=>{
+            el.content.style.transition = '0';
+            el.content.style.height = el.content.scrollHeight + "px";
+
+            requestAnimationFrame(()=>{
+              el.content.style.transition = null;
+              el.content.style.height = 0;
+            });
+          });
+        }
+
+        open(el){
+          el.selected = true;
+          this.fire("elementSelected", el);
+          this.expand(el);
+          el.wrapper.classList.add(this.selectedClass);
+        }
+
+        close(el){
+          el.selected = false;
+          this.fire("elementUnselected", el);
+          this.collapse(el);
+          el.wrapper.classList.remove(this.selectedClass);
+        }
+
+        toggle(el){
+          if(el.selected){
+            this.close(el);
+          } else {
+            this.open(el);
+
+            if(this.oneAtATime){        
+              this.els.filter(e=>e!=el && e.selected).forEach(e=>{
+                this.close(e);
+              });
+            }
+          }
+        }
+
+        attachEvents(){    
+          this.els.forEach((el, i)=>{
+            el.trigger.addEventListener("click", this.toggle.bind(this, el));
+          });
+        }
+
+        setDefaultData(){    
+          this.els = [];
+          this.events = {
+            'elementSelected': [],
+            'elementOpened': [],
+            'elementUnselected': [],
+            'elementClosed': []
+          };
+          this.transitionendevent = this.transitionendEventName();
+          this.oneAtATime = true;
+          this.selectedClass = "selected";
+          this.trigger = "[data-accordion-element-trigger]";
+          this.content = "[data-accordion-element-content]";
+        }
+
+        setCustomData(data){
+          var keys = Object.keys(data);
+
+          for(var i=0; i<keys.length; i++){
+            this[ keys[i] ] = data[ keys[i] ];
+          }
+        }
+
+        fire(eventName, el){
+          var callbacks = this.events[eventName];
+          for(var i=0; i<callbacks.length; i++){
+            callbacks[i]( el )
+          }
+        }
+
+        on(eventName, cb){
+          if( !this.events[eventName] ) return       
+          this.events[eventName].push(cb);
+        }
+
+        constructor(data){    
+          this.setDefaultData();
+          this.setCustomData(data); // ES6 => Object.assign(this, data)
+
+          [].forEach.call( document.querySelectorAll(this.elements), (el, i)=>{
+            this.els.push({
+              wrapper: el,
+              trigger: el.querySelector(this.trigger),
+              content: el.querySelector(this.content)
+            });
+
+            this.els[i].content.style.height = 0;
+          });
+
+          this.attachEvents();
+        }
+
+      }
+    })();
+
+    var myAccordion = new gianniAccordion({
+      elements: ".card article"
+    });
+
+    myAccordion.on("elementSelected", (data)=>{
+      console.log("elementOpened", data);
+    });
+</script>
+
 <?php 
 
     get_footer();
